7e5bab5195a9ca72c08a3f7ee4752f1e5c395077
Ovi Savescu <ovi@ovswebsites.com>
2021-08-02 18:10:36 +0300

    replaced gatsby-plugin-google-analytics with gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -188,14 +188,39 @@ module.exports = {
       },
     },
     "gatsby-plugin-sitemap",
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "UA-110965058-1",
+    //     // Defines where to place the tracking script - `true` in the head and `false` in the body
+    //     head: true,
+    //     // sampleRate: 5,
+    //     // siteSpeedSampleRate: 10
+    //   },
+    // },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-110965058-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // sampleRate: 5,
-        // siteSpeedSampleRate: 10
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-110965058-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: false,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     "gatsby-plugin-theme-ui",
