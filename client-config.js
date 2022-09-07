95a9849a9b909d15d860d769ea3c42631776e142
OVIS <ovi@ovswebsites.com>
2022-09-07 11:12:12 +0300

    updated from jr1ot0wt to sanity project id e4y15utr as part of credentials move

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'jr1ot0wt', // was g9m20zf8
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'e4y15utr', // was g9m20zf8
     dataset: process.env.GATSBY_SANITY_DATASET || 'production'
   }
 }
